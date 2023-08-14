e3619508951f019ee4ad7ef453a82e214b0c0d1e
Emygut <106460135+Emygut@users.noreply.github.com>
2023-08-14 01:54:44 -0300

    DICCIONARIO.py
    Ejercicios de Diccionario con codificacion Python correspondiente a la Evidencia 7.

---
+++ DICCIONARIO.py
@@ -0,0 +1,43 @@
+# Ejercicios DICCIONARIO: 
+# Crear el siguiente diccionario:
+#  Las claves serán los dni de su núcleo familiar, y el valor será SOLO el nombre de la persona. 
+# Luego deberán añadir los datos de familia ampliada (abuelos, familia política, etc)
+#  Crear un nuevo diccionario con claves autogeneradas y valores de números de teléfono.
+# Ambos deben ser mostrados.
+# Diccionario de nombres de núcleo familiar
+nucleo_familiar = {
+    "12345678A": "Juan",
+    "23456789B": "María",
+    "34567890C": "Pedro",
+    # ... otros miembros del núcleo familiar
+}
+
+# Diccionario de nombres de familia ampliada
+familia_ampliada = {
+    "11111111X": "Abuelo Juan",
+    "22222222Y": "Abuela María",
+    "33333333Z": "Tío/a Pedro",
+    # ... otros miembros de la familia ampliada
+}
+# Diccionario de números de teléfono
+numeros_telefono = {
+    "12345678A": "123456789",
+    "23456789B": "987654321",
+    "34567890C": "555555555",
+    "11111111X": "111111111",
+    "22222222Y": "222222222",
+    "33333333Z": "333333333",
+    # ... otros números de teléfono
+}
+
+# Mostrar los diccionarios
+print("Diccionario de núcleo familiar:")
+print(nucleo_familiar)
+
+print("\nDiccionario de familia ampliada:")
+print(familia_ampliada)
+
+print("\nDiccionario de números de teléfono:")
+print(numeros_telefono)
+
+# En este ejemplo, hemos creado tres diccionarios: uno para el núcleo familiar, otro para la familia ampliada y otro para los números de teléfono. Puedes agregar más claves y valores según tus necesidades.
