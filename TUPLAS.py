bf3ced745e36ae484ab5753d5c1e90d7096ee162
Emygut <106460135+Emygut@users.noreply.github.com>
2023-08-14 01:53:07 -0300

    TUPLAS.py
    Ejercicios de Tuplas con codificacion Python correspondiente a la Evidencia 7

---
+++ TUPLAS.py
@@ -0,0 +1,17 @@
+# Ejercicios Tuplas: 
+# Crear tres tuplas con datos random. 
+# Crear una lista que las contenga y mostrarla.
+import random
+
+# Tupla de nombres
+nombres = ("Juan", "María", "Carlos", "Ana", "Luis")
+print (nombres)
+# Tupla de coordenadas
+coordenadas = ((0, 0), (3, 5), (-2, 8), (10, -1))
+print (coordenadas)
+# Tupla de colores
+colores = ("rojo", "verde", "azul", "amarillo", "morado")
+print (colores)
+# Crear la lista que contiene las tuplas
+lista_de_tuplas = [nombres, coordenadas, colores]
+print (lista_de_tuplas)
